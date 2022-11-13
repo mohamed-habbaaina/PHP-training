0212d49a937d15141e35fbddcfa67b7e24eaad82
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-13 15:57:06 +0100

    tableau avec des clès

--- tableaux.php
+++ tableaux.php
@@ -1,3 +1,8 @@
 <?php 
-$notes = [12, 16, 17, 13, 18];
-echo $notes[2];
+$eleve = [
+    'nom' => 'marc',
+    'prenom' => 'lucky',
+    'classe' => '2AS',
+    'notes' => [12, 16, 17, 13, 18]
+];
+echo $eleve['notes'][4];
