de77d8915c628e0b260cec05c15f0873b32863f4
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-13 15:25:50 +0100

    affichage d'une phrase + calcule simple

---
+++ test2.php
@@ -0,0 +1,9 @@
+<?php 
+    $aml = 'marion';
+    $sss = 'goumaz';
+    $a =12;
+    $b = 18;
+    // $c = (($a+$b)/2);
+
+
+    echo 'bonjour ' . $aml . ' ' . $sss . ', vous avais eu ' . (($a+$b)/2) . ' de moyen, felicitation';
